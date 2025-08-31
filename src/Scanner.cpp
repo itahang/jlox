4ec1944cf872bd2935535e2a845af359e0c5c7ae
ihang <hangita101@gmail.com>
2025-08-31 09:23:39 +0000

    init

---
+++ src/Scanner.cpp
@@ -0,0 +1,87 @@
+#pragma once
+#include "Tokens.cpp"
+#include <vector>
+
+class Scanner
+{
+public:
+    Scanner(std::string source_) : source{source_}
+    {
+    }
+
+    std::vector<Token> scanTokens()
+    {
+        while (!isAtEnd())
+        {
+            start = current;
+            scanToken();
+        }
+        tokens.emplace_back(TokenType::EOFF, "", NULL, line);
+        return tokens;
+    }
+
+private:
+    bool isAtEnd()
+    {
+        return current >= source.length();
+    }
+
+    void void scanToken()
+    {
+        char c = advance();
+        switch (c)
+        {
+        case '(':
+            addToken(TokenType::LEFT_PAREN);
+            break;
+        case ')':
+            addToken(TokenType::RIGHT_PAREN);
+            break;
+        case '{':
+            addToken(TokenType::LEFT_BRACE);
+            break;
+        case '}':
+            addToken(TokenType::RIGHT_BRACE);
+            break;
+        case ',':
+            addToken(TokenType::COMMA);
+            break;
+        case '.':
+            addToken(TokenType::DOT);
+            break;
+        case '-':
+            addToken(TokenType::MINUS);
+            break;
+        case '+':
+            addToken(TokenType::PLUS);
+            break;
+        case ';':
+            addToken(TokenType::SEMICOLON);
+            break;
+        case '*':
+            addToken(TokenType::STAR);
+            break;
+        }
+    }
+
+    char advance()
+    {
+        return source.at(current++);
+    }
+    void addToken(TokenType type)
+    {
+        addToken(type, NULL);
+    }
+
+    void addToken(TokenType type, std::any literal)
+    {
+        std::string text = source.substr(start, current);
+        tokens.emplace_back(type, text, NULL, line);
+    }
+
+    std::string source;
+    std::vector<Token> tokens;
+    int start = 0;
+    int current = 0;
+    int line = 1;
+};
---
+++ src/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string_view>
+#include <vector>
+#include <any>
+#include <list>
+#include "Tokens.cpp"
+
+bool hasError = false;
+void report(int line, std::string where, std::string message)
+{
+    std::cerr << "Line [" << line << "] where: " << message;
+    hasError = true;
+}
+
+void error(int line, std::string message)
+{
+    report(line, "", message);
+}
+
+template <typename T = std::string>
+void run(T &&content)
+{
+    std::vector<std::string> tokens;
+    std::istringstream iss(content); // wrap string in a stream
+    std::string token;
+
+    while (iss >> token)
+    { // now >> works
+        tokens.push_back(token);
+        std::cout << token << "\n";
+    }
+}
+
+void runFile(std::string path)
+{
+    std::ifstream file(path);
+    if (!file.is_open())
+    {
+        std::cerr << "Error while opening file\n";
+        return;
+    }
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string contents = buffer.str();
+    run(std::move(contents));
+    if (hasError)
+    {
+        exit(65);
+    }
+}
+
+void runPrompt()
+{
+    std::string line;
+    for (;;)
+    {
+        std::cout << ">> ";
+        std::getline(std::cin, line);
+        if (line == ".exit")
+        {
+            break;
+        }
+        run(line);
+        hasError = false;
+    }
+}
+int main(int argc, char **argv)
+{
+    if (argc > 2)
+    {
+        std::cerr << "Usage: jlox [script]" << std::endl;
+        exit(64);
+    }
+    else if (argc == 2)
+    {
+        runFile(std::string(argv[1]));
+    }
+    else
+    {
+        runPrompt();
+    }
+}
